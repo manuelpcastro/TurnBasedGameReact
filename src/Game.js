fafa80ef36133a65d80e352af3bdb5249f9a7318
manuelpcastro <manuelpancorbocastro@gmail.com>
2021-01-17 00:42:45 +0100

    Add Game Over. Fixed most style issues

--- src/Game.js
+++ src/Game.js
@@ -10,18 +10,25 @@ import Players from './assets/players.json'
 function Game() {
 
     const [current, setCurrent] = React.useState(0);
-
+    const [cursorPlayer1, setCursor1] = React.useState(true);
+    const [cursorPlayer2, setCursor2] = React.useState(false);
+    const [ending, setEnding] = React.useState(false);
 
     const players = Players;
 
-
-
-    const next = () => setCurrent((current + 1) % 2);
+    const next = () => {
+        setCurrent((current + 1) % 2);
+        setCursor1(!cursorPlayer1);
+        setCursor2(!cursorPlayer2);
+    }
 
     const attack = () => {
         players[(current + 1) % 2].hp -= (players[current].dmg - players[(current + 1) % 2].shield);
         next();
-        players[current].shield = 0;  
+        if (players[current].hp <= 0) {
+            setEnding(true);
+        }
+        players[current].shield = 0;
     }
 
     const defend = () => {
@@ -33,15 +40,24 @@ function Game() {
     return (
         <div className="App">
 
+
             <div className="character">
+                {cursorPlayer1 && <img alt="cursor" className="selected" src="./img/cursor.png" />}
                 <Character player={players[0]} />
             </div>
 
+
             <div className="enemy">
+                {cursorPlayer2 && <img alt="cursor" className="selected" src="./img/cursor.png" />}
                 <Character player={players[1]} />
             </div>
 
-            <Menu attack={attack} defend={defend} player={players[current]} />
+
+            {ending ?
+                <div class="ending">Game Over</div>
+                :
+                <Menu attack={attack} defend={defend} player={players[current]} />
+            }
         </div>
     );
 
--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -10,7 +10,7 @@ function Character(props) {
  
     //this.state = { hp: props.player.hp, dmg: props.player.dmg, potions: ['+20', '+20', '+10'] };
   const [hp, setHP] = React.useState(0);
-
+  
   const potions= ['+20', '+20', '+10'];
 
   React.useEffect(() =>{
@@ -21,7 +21,7 @@ function Character(props) {
     <div key={props.player} className='character-container'>
       <Avatar avatar={props.player.name} />
       <div className='character-name'>
-        <h3>{props.player.name}</h3>
+        <h3>{props.player.name}{props.player.shield !== 0 && '🛡️'}</h3> 
       </div>
       <Stats className='stats' hp={hp} dmg={props.player.dmg} />
       <Items potions={potions} />
