10806b14248a1a9ff7542014d04197257eb88d69
manuelpcastro <manuelpancorbocastro@gmail.com>
2021-01-16 22:05:48 +0100

    Fix img path for deployment

--- src/components/Character/Avatar.js
+++ src/components/Character/Avatar.js
@@ -2,7 +2,7 @@ import React from "react";
 import './Character.css';
 
 const Avatar = props => (
-    <img className='char-icon' src={`img/${props.avatar}.png`} alt="avatar" />
+    <img className='char-icon' src={`./img/${props.avatar.toLowerCase()}.png`} alt="avatar" />
 );
 
 export default Avatar;
