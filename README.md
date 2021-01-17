ce531428ef9e8e623838573b0435bd39087a9d92
manuelpcastro <manuelpancorbocastro@gmail.com>
2021-01-17 13:33:24 +0100

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-Just a Playground Project to learn some React basics.
-
-I wanted to do a simple game, so I thought that a turn-based combat game could be fine to start using React.
-
--- src/components/Character/Avatar.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './Character.css';
-
-const Avatar = props => (
-    <img className='char-icon' src={`./img/${props.avatar.toLowerCase()}.png`} alt="avatar" />
-);
-
-export default Avatar;
